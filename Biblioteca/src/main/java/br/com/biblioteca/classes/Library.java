c114686831ca90f83de8f4c9a5632623a3ff403c
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-07-09 20:19:22 -0300

    test: create test library is void ?

--- Biblioteca/src/main/java/br/com/biblioteca/classes/Library.java
+++ Biblioteca/src/main/java/br/com/biblioteca/classes/Library.java
@@ -1,5 +1,5 @@
 package br.com.biblioteca.classes;
 
 public class Library {
-
+    public boolean isVoid;
 }
--- Biblioteca/src/test/java/LibraryTest.java
+++ Biblioteca/src/test/java/LibraryTest.java
@@ -1,2 +1,18 @@
+import br.com.biblioteca.classes.Library;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class LibraryTest {
+    protected Library library;
+
+    @Before
+    public void before() {
+        library = new Library();
+    }
+
+    @Test
+    public void isVoidTest() {
+        Assert.assertTrue(library.isVoid);
+    }
 }
