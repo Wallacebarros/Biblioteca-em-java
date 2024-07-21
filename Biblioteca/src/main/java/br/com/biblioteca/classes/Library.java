29ec0895e033cf751e1f6127d13d4ae4bb98e4be
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-07-21 16:41:22 -0300

    adicionando mais funcionalidades e testes a class Library

--- Biblioteca/src/main/java/br/com/biblioteca/classes/Library.java
+++ Biblioteca/src/main/java/br/com/biblioteca/classes/Library.java
@@ -1,7 +1,28 @@
 package br.com.biblioteca.classes;
 
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books = new ArrayList<>(20);
+
     public boolean isVoid() {
-        return true ;
+        return books.isEmpty();
+    }
+
+    public void addBook(Book newBook) {
+        books.add(newBook);
+    }
+
+    public int totalBooks() {
+        return books.size();
+    }
+
+    public ArrayList<Book> findAllBooks() {
+        ArrayList<Book> copyBooks = books;
+        return copyBooks;
+    }
+
+    public void deletBook(Book book) {
+        books.remove(book);
     }
 }
--- Biblioteca/src/test/java/LibraryTest.java
+++ Biblioteca/src/test/java/LibraryTest.java
@@ -1,18 +1,54 @@
+import br.com.biblioteca.classes.Book;
 import br.com.biblioteca.classes.Library;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class LibraryTest {
-    protected Library library;
+    private Library library;
+    private Book book1, book2;
 
     @Before
     public void before() {
         library = new Library();
+        book1 = new Book("Book 1", "Author 1", 150);
+        book2 = new Book("Book 2", "Author 2", 200);
     }
 
     @Test
     public void isVoidTest() {
         Assert.assertTrue(library.isVoid());
+    }
+
+    @Test
+    public void addBook() {
+        library.addBook(book1);
+        Assert.assertFalse(library.isVoid());
+    }
+
+    @Test
+    public void deletBook() {
+        library.addBook(book1);
+        library.deletBook(book1);
+        Assert.assertTrue(library.isVoid());
+    }
+
+    @Test
+    public void totalBooks() {
+        library.addBook(book1);
+        library.addBook(book2);
+        Assert.assertEquals(2, library.totalBooks());
+    }
+
+    @Test
+    public void findAllBooks() {
+        library.addBook(book1);
+        library.addBook(book2);
+        ArrayList<Book> a = new ArrayList<>(20);
+        a.add(book1);
+        a.add(book2);
+        Assert.assertEquals(a, library.findAllBooks());
     }
 }
